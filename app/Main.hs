341de8d17fb9adb205cf834bd4b9bcb468a94dd4
Joseph Weston <joseph@weston.cloud>
2019-11-20 23:26:54 +0100

    add bare-bones executable for parsing MT940 messages from a file

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import qualified Data.ByteString as BS
+import Text.Megaparsec
+import Data.ABNAmro.MT940 hiding (parse)
+
+import System.Environment
+
 main :: IO ()
-main = pure ()
+main = do
+    fileName <- head <$> getArgs
+    print . parse messages fileName =<< BS.readFile fileName
+    where
+        messages = some parser :: Parser [MT940Message]
